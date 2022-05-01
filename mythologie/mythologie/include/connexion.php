51ff69cf76f9d6fefd10baa75cf405151e6d9aa7
DANLCARTON <78015609+DANLCARTON@users.noreply.github.com>
2022-05-01 19:35:13 +0200

    Lieux

---
+++ mythologie/mythologie/include/connexion.php
@@ -0,0 +1,16 @@
+<?php
+
+    function connexion() {
+                $pdo = new PDO("mysql:host=sqletud.u-pem.fr;dbname=charline.le-pape_db", "charline.le-pape" , "123");
+                //$pdo = new PDO('mysql:host=localhost;dbname=mythologie;charset=utf8', 'root', '');
+                $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+            
+                if ($pdo) {
+                    // echo '<p>Connexion réussie</p>';
+                    return $pdo;
+                }
+                else {
+                    echo '<p>Erreur de connexion</p>';
+                    exit;
+                }
+            }
---
+++ mythologie/mythologie/vues/lieu_affichage.php
@@ -0,0 +1,31 @@
+<?php
+
+function affiche_liste_lieux($lieux){
+    foreach ($lieux as $lieu){
+        echo    '<h3 class="lieu">
+                    <a href="./index.php?action=page_detail_lieu&id_lieu='.$lieu['id_perso'].'"> '.$lieu['nom_perso'].'</a>
+                </h3>'.
+
+        //A limiter le nombre de caractère pour le lieu
+
+                '<p>'.$lieu['nom_lieu'].'</p>
+                <p>'.$lieu['desc_lieu'].'</p>';
+    }
+
+    // Bouton ajouter un lieu
+
+    echo '<a href="./index.php?action=page_lieu/add">Ajouter un lieu ?</a>';
+}
+
+function affiche_lieu($lieu) {
+
+    echo    "<h3 class='nom'>".$lieu['nom_lieu']."</h3>
+            <p class='desc'>".$lieu['desc_lieu']."</p>
+            <img src='images/upload".$lieu['illu_lieu']."'alt='photo'>".
+
+            // Bouton supprimer
+
+            "<a href='./index.php?action=page_detail_lieu&id_lieu=".$lieu['id_lieu']."/delete'>Supprimer ?</a>";
+}
+
+?>
