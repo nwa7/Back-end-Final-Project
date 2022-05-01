cd15436428bc10dff1bd370a7f63d5e4719cb4f6
DANLCARTON <78015609+DANLCARTON@users.noreply.github.com>
2022-05-01 20:23:21 +0200

    Add files via upload

---
+++ mythologie/vues/categorie_affichage.php
@@ -0,0 +1,25 @@
+<?php
+
+function affiche_liste_categorie($categories){
+
+    foreach ($categories as $categorie){
+        echo '<h3 class="mythologie"><a href="./index.php?action=page_detail_mythologie&id_cat='.$categorie['id_cat'].
+        '"> '.$categorie['nom_cat'].'</a></h3>';
+        //A limiter le nombre de caractère pour le mythe
+        echo '<p>'.$categorie['desc_cat'].'</p>';
+    }
+
+}
+
+function affiche_categorie($categorie) {
+
+    echo    "<h3 class='nom'>".$categorie['nom_cat']."</h3>
+            <p class='desc'>".$categorie['desc_cat']."</p>
+            <img src='images/upload".$categorie['illu_cat']."'alt='photo'>".
+
+            // Bouton supprimer
+
+            "<a href='./index.php?action=page_detail_categorie&id_categorie=".$categorie['id_categorie']."/delete'>Supprimer ?</a>";
+}
+
+?>
