26a21f3d519aabfff5404dc539a5e6b65e5cc2f2
maxime <mathevet.max@gmail.com>
2022-05-10 10:24:13 +0200

    maxime add delete form

---
+++ mythologie/include/categorie/categorie_add.php
@@ -0,0 +1,21 @@
+<?php
+
+    function add_categorie($pdo, $nom_cat, $desc_cat, $illu_cat) {
+        $sql ="INSERT INTO `categorie` (id_cat, nom_cat, desc_cat, illu_cat) VALUES (DEFAULT, \"$nom_cat\", \"$desc_cat\", \"$illu_cat\")";
+        
+        $query = $pdo->prepare($sql);
+        $query->execute();
+
+        if($query->errorCode() == '00000') {
+            $ligne = $query->fetch(PDO::FETCH_ASSOC);
+        }
+
+        else {
+            echo '<p>Erreur dans la requête</p>';
+            echo '<p>'.$query->errorInfo()[2].'</p>';
+            $tableau = null;
+        }
+
+        // récupération de la première ligne (unique)  
+        return $ligne;
+    }
---
+++ mythologie/vues/categorie/categorie_form_add.php
@@ -0,0 +1,7 @@
+<h2> Nouvelle catégorie </h2>
+<form target="_self" method="POST">
+    <label>Nom : </label><input type="text" name="nom_cat" placeholder="Entrez son nom" required><br>
+    <label>Description : </label><textarea name="desc_cat" rows="5" cols="30">Entrez sa description</textarea><br>
+    <label>Illustration : </label><!-- <input type="file" accept="image/png, image/jpeg, image/gif, image/webp" name="illu_cat"> ???? --><br>
+    <input type="submit" value="OK"></p>
+</form>
