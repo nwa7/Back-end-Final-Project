86b6f9649557f9072f227a4515dc517b24334761
Elia Roard <eliaroard@gmail.com>
2022-05-17 12:14:01 +0200

    fixed race

--- mythologie/include/race/race_select.php
+++ mythologie/include/race/race_select.php
@@ -48,21 +48,6 @@
       return $ligne;
   }
   
-  function select_persos_race($pdo,$id_race) {
-      $sql = 'SELECT personnage.nom_perso FROM race JOIN personnage ON personnage.id_race=race.id_race WHERE id_race=:id_race';
-      $query = $pdo->prepare($sql);
-      $query->bindValue(':id_race',$id_race,PDO::PARAM_STR);
-      $query->execute();
-      if($query->errorCode() == '00000') {
-          $tableau = $query->fetchAll(PDO::FETCH_ASSOC);
-      }
-      else {
-      echo '<p>Erreur dans la requête</p>';
-      echo '<p>'.$query->errorInfo()[2].'</p>';
-      $tableau = null;
-      }
-      return $tableau;
-  }
 
   function select_form_races($races){
     echo '<select name="id_race" size=1 >';
